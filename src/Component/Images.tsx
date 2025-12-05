d64e399f0f413bf33d009d2f9cab0fb8c86d42af
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-12-05 20:07:12 +0530

    final commmit

--- src/Component/Images.tsx
+++ src/Component/Images.tsx
@@ -17,48 +17,56 @@ function Images() {
   }
 
   return (
-    <div className="relative w-[95px] h-[76px] overflow-hidden">
+    <div className="relative w-[95px] h-[76px]">
 
-      <div className="group absolute border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden top-0 left-0 translate-x-[4px] translate-y-[4px] z-20">
-        <img
-          onClick={() => handleclick(Data[0])}
-          src={getSrc(Data[0])}
-          className="object-cover w-full h-full scale-[1.3]"
-        />
-        <span className="absolute left-1/2 -top-6 -translate-x-1/2 px-2 py-1 text-xs bg-black text-white rounded opacity-0 group-hover:opacity-100 transition">
+      <div className="group absolute top-0 left-0 translate-x-[4px] translate-y-[4px] z-20">
+        <div className="border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden">
+          <img
+            onClick={() => handleclick(Data[0])}
+            src={getSrc(Data[0])}
+            className="object-cover w-full h-full scale-[1.3]"
+          />
+        </div>
+        <span className="absolute left-1/2 -top-6 -translate-x-1/2 px-2 py-1 text-xs bg-white text-black rounded opacity-0 z- group-hover:opacity-100 transition">
           {Data[0]?.name}
         </span>
       </div>
 
-      <div className="group absolute border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden top-0 right-0 -translate-x-[4px] translate-y-[4px] z-10">
-        <img
-          onClick={() => handleclick(Data[1])}
-          src={getSrc(Data[1])}
-          className="object-cover w-full h-full scale-[1.3]"
-        />
-        <span className="absolute left-1/2 -top-6 -translate-x-1/2 px-2 py-1 text-xs bg-black text-white rounded opacity-0 group-hover:opacity-100 transition">
+      <div className="group absolute top-0 right-0 -translate-x-[4px] translate-y-[4px] z-10">
+        <div className="border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden">
+          <img
+            onClick={() => handleclick(Data[1])}
+            src={getSrc(Data[1])}
+            className="object-cover w-full h-full scale-[1.3]"
+          />
+        </div>
+        <span className="absolute left-1/2 -top-6 -translate-x-1/2 px-2 py-1 text-xs bg-white text-black rounded opacity-0 z- group-hover:opacity-100 transition">
           {Data[1]?.name}
         </span>
       </div>
 
-      <div className="group absolute border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden top-0 left-0 translate-x-[4px] translate-y-[32px] z-10">
-        <img
-          onClick={() => handleclick(Data[2])}
-          src={getSrc(Data[2])}
-          className="object-cover w-full h-full scale-[1.3]"
-        />
-        <span className="absolute left-1/2 -top-6 -translate-x-1/2 px-2 py-1 text-xs bg-black text-white rounded opacity-0 group-hover:opacity-100 transition">
+      <div className="group absolute top-0 left-0 translate-x-[4px] translate-y-[32px] z-10">
+        <div className="border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden">
+          <img
+            onClick={() => handleclick(Data[2])}
+            src={getSrc(Data[2])}
+            className="object-cover w-full h-full scale-[1.3]"
+          />
+        </div>
+        <span className="absolute left-1/2 top-10 -translate-x-1/2 px-2 py-1 text-xs bg-white text-black rounded opacity-0 z- group-hover:opacity-100 transition">
           {Data[2]?.name}
         </span>
       </div>
 
-      <div className="group absolute border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden top-0 right-0 -translate-x-[4px] translate-y-[32px] z-0">
-        <img
-          onClick={() => handleclick(Data[3])}
-          src={getSrc(Data[3])}
-          className="object-cover w-full h-full scale-[1.3]"
-        />
-        <span className="absolute left-1/2 -top-6 -translate-x-1/2 px-2 py-1 text-xs bg-black text-white rounded opacity-0 group-hover:opacity-100 transition">
+      <div className="group absolute top-0 right-0 -translate-x-[4px] translate-y-[32px] z-0">
+        <div className="border-[1px] border-black w-[38px] h-[38px] rounded-full overflow-hidden">
+          <img
+            onClick={() => handleclick(Data[3])}
+            src={getSrc(Data[3])}
+            className="object-cover w-full h-full scale-[1.3]"
+          />
+        </div>
+        <span className="absolute left-1/2 top-10 -translate-x-1/2 px-2 py-1 text-xs bg-white text-black rounded opacity-0 z- group-hover:opacity-100 transition">
           {Data[3]?.name}
         </span>
       </div>
